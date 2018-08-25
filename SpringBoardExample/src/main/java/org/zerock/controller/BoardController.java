f333d0ee8f5cba4f2c7b48eff374c3bb8088deac
moonyoungJo <jomy9649@gmail.com>
2018-08-25 10:50:39 +0900

    developing paging process

--- SpringBoardExample/src/main/java/org/zerock/controller/BoardController.java
+++ SpringBoardExample/src/main/java/org/zerock/controller/BoardController.java
@@ -6,11 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageMaker;
 import org.zerock.service.BoardService;
 
 @Controller
@@ -64,4 +67,21 @@ public class BoardController {
 		rttr.addFlashAttribute("msg", "SUCCESS");
 		return "redirect:/board/listAll";
 	}
+	@RequestMapping(value="/listCri", method=RequestMethod.GET)
+	public void listCri(Criteria cri, Model model) throws Exception{
+		logger.info("show list Page with Criteria.........");
+		model.addAttribute("list", service.listCriteria(cri));
+	}
+	@RequestMapping(value="/listPage", method=RequestMethod.GET)
+	public void listPage(@ModelAttribute("cri") Criteria cri, Model model) throws Exception{
+		logger.info(cri.toString());
+		
+		model.addAttribute("list", service.listCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(service.listCounrCriteria(cri));
+		
+		model.addAttribute("pageMaker", pageMaker);
+	}
+	
 }
--- SpringBoardExample/src/test/java/org/zerock/test/BoardDAOTest.java
+++ SpringBoardExample/src/test/java/org/zerock/test/BoardDAOTest.java
@@ -1,5 +1,7 @@
 package org.zerock.test;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -8,7 +10,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,8 +26,38 @@ public class BoardDAOTest {
 	@Test
 	public void testCreate() throws Exception{
 		BoardVO board = new BoardVO();
-		board.setTitle("새로운 글을 넣습니다.");
+		board.setTitle("hihi");
 		board.setWriter("user00");
 		dao.create(board);
+	}
+	@Test
+	public void testListPage() throws Exception{
+		int page = 3;
+		List<BoardVO> list = dao.listPage(page);
+		
+		for(BoardVO boardVO : list)
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+	}
+	@Test
+	public void testListCriterai() throws Exception{
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+		
+		List<BoardVO> list = dao.listCriteria(cri);
+		for(BoardVO boardVO : list){
+			logger.info(boardVO.getBno() + " : " + boardVO.getTitle());
+		}
+	}
+	@Test
+	public void testURI() throws Exception{
+		UriComponents uriComponents = 
+				UriComponentsBuilder.newInstance()
+				.path("/board/read")
+				.queryParam("bno",  12)
+				.queryParam("perPageNum", 20)
+				.build();
+		logger.info("/board/read?bno=12&perPageNum=20");
+		logger.info(uriComponents.toString());
 	}
 }
